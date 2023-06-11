ee70842f4ca1af0e4ddd0b20af7966146b06196c
INHWA JO <64248143+inhwa1025@users.noreply.github.com>
2023-06-11 18:27:35 +0900

    Update main.py

--- dags/main.py
+++ dags/main.py
@@ -4,8 +4,8 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 
 def load_config(spark_context: SparkContext):
-    spark_context._jsc.hadoopConfiguration().set('fs.s3a.access.key', 'myaccesskey')
-    spark_context._jsc.hadoopConfiguration().set('fs.s3a.secret.key', 'mysecretkey')
+    spark_context._jsc.hadoopConfiguration().set('fs.s3a.access.key', 'ROOTUSER')
+    spark_context._jsc.hadoopConfiguration().set('fs.s3a.secret.key', 'CHANGEME123')
     spark_context._jsc.hadoopConfiguration().set('fs.s3a.path.style.access', 'true')
     spark_context._jsc.hadoopConfiguration().set('fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')
     spark_context._jsc.hadoopConfiguration().set('fs.s3a.endpoint', 'http://133.186.150.141:9000') // minio의 외부에 공개된 port 설정
