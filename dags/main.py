e8c24c895240f56ab36cc724a1d5ed4151290909
INHWA JO <64248143+inhwa1025@users.noreply.github.com>
2023-06-11 21:06:35 +0900

    Update main.py

--- dags/main.py
+++ dags/main.py
@@ -8,13 +8,13 @@ def load_config(spark_context: SparkContext):
     spark_context._jsc.hadoopConfiguration().set('fs.s3a.secret.key', 'CHANGEME123')
     spark_context._jsc.hadoopConfiguration().set('fs.s3a.path.style.access', 'true')
     spark_context._jsc.hadoopConfiguration().set('fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')
-    spark_context._jsc.hadoopConfiguration().set('fs.s3a.endpoint', 'http://133.186.150.141:9000') // minio의 외부에 공개된 port 설정
+    spark_context._jsc.hadoopConfiguration().set('fs.s3a.endpoint', 'http://133.186.150.141:9000') # minio의 외부에 공개된 port 설정
     spark_context._jsc.hadoopConfiguration().set('fs.s3a.connection.ssl.enabled', 'false')
 
 load_config(spark.sparkContext)
 
-dataframe = spark.read.json('s3a://test/*') //orders.json 파일 선택
+dataframe = spark.read.json('s3a://test/*') # orders.json 파일 선택
 
-average = dataframe.agg({'amount': 'avg'}) //orderjs.json 파일의 amount의 평균값
+average = dataframe.agg({'amount': 'avg'}) # orderjs.json 파일의 amount의 평균값
 
 average.show()
