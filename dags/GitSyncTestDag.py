2e6594badbadd19917740ecf80ebd67db1a89d33
INHWA JO <64248143+inhwa1025@users.noreply.github.com>
2023-06-12 10:36:04 +0900

    Update GitSyncTestDag.py

--- dags/GitSyncTestDag.py
+++ dags/GitSyncTestDag.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 
 
 default_args = {
-    'description': 'Redis 새로운 버전 감지 및 Variable 크롤링',
+    'description': 'Git Sync Test',
     'depend_on_past': False,
     'start_date': days_ago(1),
     'retries': 3,
@@ -20,7 +20,7 @@ default_args = {
 
 dag = DAG(
     # DAG의 이름
-    'dynamic_task_test_0322',
+    'gitsync_task_test_0522',
     # DAG에서 사용할 기본적인 파라미터 값
     default_args=default_args,
     # DAG가 언제 실행 될 지 설정. "@once" : 한 번만 실행
@@ -38,11 +38,18 @@ end_dag = DummyOperator(
     dag=dag
 )
 
-print_pwd = BashOperator(
-    task_id='print_pwd',
+t1 = BashOperator(
+    task_id='spark-test',
     bash_command='echo $pwd',
     do_xcom_push=True,
     dag=dag
 )
 
-start_dag >> print_pwd >> end_dag
+t2 = BashOperator(
+    task_id='spark-log',
+    bash_command='echo $pwd',
+    do_xcom_push=True,
+    dag=dag
+)
+
+start_dag >> t1 >> t2 >> end_dag
